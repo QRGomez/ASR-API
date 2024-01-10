3acfe251255991eaa8b42fd8a04a4dd9fdcd85e9
QRGomez <70466036+QRGomez@users.noreply.github.com>
2024-01-10 18:15:25 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
-from typing import Optional
+import torchaudio
+import speechbrain as sb
+from speechbrain.pretrained import EncoderDecoderASR
+from fastapi import FastAPI, File, UploadFile 
+import os
+import time
 
-from fastapi import FastAPI
+app = FastAPI() #uvicorn main:app --reload (This runs starts a local instance of the API)
 
-app = FastAPI()
-
-
-@app.get("/")
+@app.get('/')
 async def root():
-    return {"message": "Hello World"}
+    return {
+        'ASR API': 'Active'
+    }
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+
+@app.post(f'/transcribe')
+async def transcribe_audio(file: UploadFile = File(...)):
+    try:
+        # Save the uploaded file to the audios directory
+        file_path = file.filename
+
+        with open(file_path, 'wb') as file_output:
+            file_output.write(file.file.read())
+
+        # Transcribe the audio using the ASR model
+        asr_model = EncoderDecoderASR.from_hparams(
+            source="speechbrain/asr-crdnn-rnnlm-librispeech",
+            savedir="pretrained_models/asr-crdnn-rnnlm-librispeech",
+        )
+        transcription = asr_model.transcribe_file(file_path)
+
+        return {"Transcription": transcription}
+
+    except Exception as e:
+        return {"error": f"Error processing file: {str(e)}"}
